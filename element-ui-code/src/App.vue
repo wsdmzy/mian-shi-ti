057e03ed129294c5c490426e2226a08fe7d7e4cc
wsdmzy <949328973@qq.com>
2020-03-15 22:08:45 +0800

    element-ui学习

---
+++ element-ui-code/src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+
+    <!-- button -->
+    <!-- <el-button type="success"
+     @click="upload"
+     loading
+     round plain  icon="el-icon-edit">
+     编辑按钮
+  </el-button>
+  <el-button type="text">文本</el-button>
+  <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button> -->
+
+  <!-- 容器式组件 -->
+  <!-- <el-button-group>
+    <el-button type="primary" icon="el-icon-arrow-left">上一页</el-button>
+    <el-button type="primary">下一页<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+  </el-button-group>   -->
+
+  <!-- <el-button-group>
+    <el-button type="primary" icon="el-icon-edit"></el-button>
+    <el-button type="primary" icon="el-icon-share"></el-button>
+    <el-button type="primary" icon="el-icon-delete"></el-button>
+  </el-button-group> -->
+
+  <!-- badge -->
+  <el-badge :value="12"  class="item">
+    <el-button size="small">评论</el-button>
+  </el-badge>
+  <el-badge :value="12" :max="9" class="item">
+    <el-button size="small">评论</el-button>
+  </el-badge>
+  <el-badge value="new" type="primary"   class="item">
+    <el-button size="small">评论</el-button>
+  </el-badge>
+
+  <el-badge is-dot class="item" :hidden="isHideDot">
+    数据
+  </el-badge>
+
+  </div>
+</template>
+
+<script>
+
+import ElButton from './components/ElButton'
+import ElButtonGroup from './components/ElButtonGroup'
+import ElBadge from './components/ElBadge'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      isHideDot: false
+    }
+  },
+  components: {
+    ElButton,
+    ElButtonGroup,
+    ElBadge
+  },
+  methods: {
+    upload(e) {
+      console.log(e)
+      // console.log(a)
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isHideDot = true
+    },3000)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.item {
+  margin-right: 20px;
+}
+</style>
---
+++ element-ui-code/src/components/ElButton.vue
@@ -0,0 +1,65 @@
+<template>
+    <button
+      class="el-button"
+      :type="nativeType"
+      :autofocus="autofocus"
+      @click="handleClick"
+      :class="[
+        type ? 'el-button--'+type : '',
+        buttonSize ? 'el-button--' + buttonSize : '',
+        {
+         'is-round': round,
+         'is-plain': plain,
+         'is-loading': loading
+        }
+      ]"
+      >
+    <i class="el-icon-loading" v-if="loading"></i>
+    <i v-if="icon && !loading" :class="icon"></i>
+    <!-- icon 光按钮 按钮slot-->
+    <span v-if="$slots.default"><slot></slot></span>
+    </button>
+
+</template>
+
+<script>
+export default {
+  name: 'ElButton',
+  props: {
+    loading: Boolean,
+    size: String,
+    type: {
+      type: String,
+      default: 'default'
+    },
+    nativeType: {
+      type: String,
+      // default: submit
+    },
+    round: {
+      type: Boolean,
+      default: false
+    },
+    plain: {
+      type: Boolean,
+      default: false
+    },
+    icon: String,
+    autofocus: Boolean
+  },
+  computed: {
+    buttonSize() {
+      return this.size || (this.$ELEMENT || {}).size;
+    }
+  },
+  methods: {
+    handleClick(evt) {
+      this.$emit('click',evt)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
